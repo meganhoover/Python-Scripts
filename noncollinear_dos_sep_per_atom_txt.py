2aa6762f83d861fbbc950d8c184483b7bae45d66
Megan Hoover <mel2@g.clemson.edu>
2021-08-29 21:41:11 -0400

    Update noncollinear_dos_sep_per_atom_txt.py

--- noncollinear_dos_sep_per_atom_txt.py
+++ noncollinear_dos_sep_per_atom_txt.py
@@ -8,13 +8,13 @@ def chunks(lst, n):
 
 # Collect all lines, without loading whole file into memory
 lines = []
-with open('/home/mel2/Python_Noncollinear_DOS/DOSCAR_datafile') as main_file:
+with open('directory path/filename') as main_file:
     for line in main_file:
         lines.append(line)
 
 # Write each group of lines to separate files
 for i, group in enumerate(chunks(lines, n=int(b+1)), start=1):
-    with open('/home/mel2/Python_Noncollinear_DOS/DOSCAR_datafile_%d.txt' % i, mode="w") as outfile:
+    with open('directory path/filename_%d.txt' % i, mode="w") as outfile:
         for line in group:
             # change all whitespace in each line to only space
             outfile.write(re.sub('[\t ]+',' ',line))
